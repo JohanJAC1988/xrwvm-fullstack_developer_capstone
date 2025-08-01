5fa7f9f73b74ba206ecfb1c3e7855fcc445694d9
JohanJAC1988 <johanjacm@gmail.com>
2025-08-01 01:39:16 -0500

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,26 +1,28 @@
-"""djangoproj URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
-from django.conf.urls.static import static
-from django.conf import settings
+Aplicación URLs (a nivel de aplicación 'dealerships')
+
+Aquí defines las rutas específicas de tu aplicación.
+"""
+from django.urls import path
+from . import views
+
+# Define el nombre de la aplicación para evitar conflictos de nombres
+app_name = 'dealerships'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # Ruta para la página de inicio que muestra la lista de concesionarios
+    path('', views.get_dealerships, name='index'),
+
+    # Ruta para la página 'acerca de'
+    path('about/', views.about, name='about'),
+
+    # Ruta para la página de 'contacto'
+    path('contact/', views.contact, name='contact'),
+
+    # Ruta para ver los detalles de un concesionario y sus reseñas
+    # El <int:dealer_id> es un parámetro dinámico que captura el ID del concesionario
+    path('<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+
+    # Ruta para el formulario de publicación de una nueva reseña
+    path('postreview/<int:dealer_id>/', views.post_review, name='post_review'),
+]
